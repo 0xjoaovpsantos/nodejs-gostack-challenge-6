f4324bbd90c0634d7e3fb533e5692c8e481450f6
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-04 15:02:41 -0300

    finish challenge

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Category from './Category';
+import AddCategoryIdToTransactions1588599838333 from '../database/migrations/1588599838333-AddCategoryIdToTransactions';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
